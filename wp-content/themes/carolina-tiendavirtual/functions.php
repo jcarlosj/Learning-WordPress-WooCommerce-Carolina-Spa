50af97171d3689697e2ba1b862b7b00c8882f7af
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-05-07 10:23:47 -0500

    Cambia prioridad de visualización del precio del producto usando 'Hooks'

--- wp-content/themes/carolina-tiendavirtual/functions.php
+++ wp-content/themes/carolina-tiendavirtual/functions.php
@@ -1,3 +1,4 @@
 <?php
-    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );        # Elimina el Título del producto 
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );    # Elimina el precio haciendo uso del hook.
+    add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 1 );        # Agrega el precio haciendo uso del hook y estableciendo una nueva prioridad.
 ?>
