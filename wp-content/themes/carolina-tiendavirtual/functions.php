166aafbbb97829d10f696be2a9faa11a0bc97ba2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-05-07 10:39:59 -0500

    Limita cantidad de productos por página del carrito usando 'Hooks'

--- wp-content/themes/carolina-tiendavirtual/functions.php
+++ wp-content/themes/carolina-tiendavirtual/functions.php
@@ -1,4 +1,12 @@
 <?php
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );    # Elimina el precio haciendo uso del hook.
     add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 1 );        # Agrega el precio haciendo uso del hook y estableciendo una nueva prioridad.
+
+    /* Limita la cantidad de productos que se van a mostrar en la tienda por página */
+    function productos_por_pagina( $cantidad ) {
+        $cantidad = 6;
+        return $cantidad;
+    }
+    add_filter( 'loop_shop_per_page', 'productos_por_pagina', 20 );
+
 ?>
