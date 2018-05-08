988cacdc3548e426a2658aed3730f93d67bc22af
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-05-08 10:21:17 -0500

    Aumenta a 16 productos por página, despliega 4 columnas en la 'Tienda'

--- wp-content/themes/carolina-tiendavirtual/functions.php
+++ wp-content/themes/carolina-tiendavirtual/functions.php
@@ -2,12 +2,19 @@
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );    # Elimina el precio haciendo uso del hook.
     add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 1 );        # Agrega el precio haciendo uso del hook y estableciendo una nueva prioridad.
 
-    /* Limita la cantidad de productos que se van a mostrar en la tienda por página */
-    function productos_por_pagina( $cantidad ) {
-        $cantidad = 6;
-        return $cantidad;
+    # Limita la cantidad de productos que se van a mostrar en la tienda por página
+    function carolina_spa_productos_por_pagina( $productos ) {
+        $productos = 16;
+        return $productos;
     }
-    add_filter( 'loop_shop_per_page', 'productos_por_pagina', 20 );
+    add_filter( 'loop_shop_per_page', 'carolina_spa_productos_por_pagina', 20 );
+
+    # Cantidad de columnas de productos por pagína (Tienda)
+    function carolina_spa_columnas( $columnas ) {
+        $columnas = 4;
+        return $columnas;
+    }
+    add_action( 'loop_shop_columns', 'carolina_spa_columnas', 20 );
 
     # Cambia simbolo por código de divisa (abreviatura tres caracteres) correspondiente a moneda establecida en los ajustes del sitio, por ejemplo: pesos colombianos (COP)
     function carolina_spa_moneda( $simbolo, $moneda ) {
