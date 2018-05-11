92bb7d5e96efe0a9a097ba8f027dc8ebada11c82
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-05-11 13:26:47 -0500

    Aplica estilos sección de ventajas (íconos y descripciones) en el 'Homepage'

--- wp-content/themes/carolina-tiendavirtual/functions.php
+++ wp-content/themes/carolina-tiendavirtual/functions.php
@@ -2,7 +2,7 @@
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );    # Elimina el precio haciendo uso del hook.
     add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 1 );        # Agrega el precio haciendo uso del hook y estableciendo una nueva prioridad.
 
-    # Imprime íconos y descripciones de ventajas de la tienda con ACF (Advanced Custom Fields)
+    # Imprime sección de ventajas (íconos y descripciones) de la tienda con ACF (Advanced Custom Fields)
     function carolina_spa_iconos_descripcion_ventajas() {
         $ventajas = array(
             'envio_gratuito' => array(
