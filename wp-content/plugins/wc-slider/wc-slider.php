df292f76ad6a93c66292c0187e9e35fb47faf88d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-05-13 09:44:50 -0500

    Crea archivo base para 'Plugin' personalizado

---
+++ wp-content/plugins/wc-slider/wc-slider.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Plugin Name: BxSlider para WordPress
+    Plugin URI:
+    Description: Agrega BxSlider a WooCommerce (Personalizado)
+    Version: 1.0.0
+    Author: Juan Carlos Jiménez Gutiérrez
+    Author URI:
+    Text Domain:
+    Domain Path:
+*/
+
+// No permitir que este se cargue directamente
+defined( 'ABSPATH' ) or die( 'No Script kiddies please!' );
