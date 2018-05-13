361fee978beda30efcd7a097b7633fbd042ea100
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-05-13 11:10:25 -0500

    Ejecuta la funcionalidad 'jQuery library BxSlider' y Refactoriza

--- wp-content/plugins/wc-slider/wc-slider.php
+++ wp-content/plugins/wc-slider/wc-slider.php
@@ -18,7 +18,7 @@ define( 'WC_SLIDER_PATH', plugin_dir_url( __FILE__ ) );
 
 # Cargar Scripts y StyleSheets
 function wc_slider_scripts() {
-    wp_enqueue_style( 'bxslider-css', WC_SLIDER_PATH. '/assets/css/jquery.bxslider.min.css', '', '4.2.1' );    # Agrega CSS de la librería BxSlider
+    wp_enqueue_style( 'bxslider-css', WC_SLIDER_PATH. 'assets/css/jquery.bxslider.min.css', '', '4.2.1' );    # Agrega CSS de la librería BxSlider
 
     # Valida el script de 'jQuery' ya está registrado o cargado en la página
     if( wp_script_is( 'jquery', 'enqueued' ) ) {
@@ -28,7 +28,7 @@ function wc_slider_scripts() {
         wp_enqueue_script( 'jquery' );
     }
 
-    wp_enqueue_script( 'bxslider-js', WC_SLIDER_PATH. '/assets/js/jquery.bxslider.min.js', '', '4.2.1'  );
+    wp_enqueue_script( 'bxslider-js', WC_SLIDER_PATH. 'assets/js/jquery.bxslider.min.js', '', '4.2.1'  );
 }
 add_action( 'wp_enqueue_scripts', 'wc_slider_scripts' );
 
@@ -41,7 +41,7 @@ function wc_slider_shortcode() {
     );
 
     $slider_productos = new WP_Query( $args );
-    echo '<ul>';
+    echo '<ul class="slider">';
 
     while( $slider_productos -> have_posts() ): $slider_productos -> the_post();
         ?>
@@ -69,3 +69,15 @@ function wc_slider_shortcode() {
 
 }
 add_shortcode( 'wc-slider', 'wc_slider_shortcode' );
+
+# Implementa el script que despliega la funcionalidad del Slider con la librería BxSlider de jQuery
+function wc_slider_execute_script() { ?>
+    <script type="text/javascript">
+        $ = jQuery .noConflict();
+        $( document ) .ready( function() {
+            $( '.slider' ) .bxSlider();
+        });
+    </script>
+    <?php
+}
+add_action( 'wp_footer', 'wc_slider_execute_script' );
